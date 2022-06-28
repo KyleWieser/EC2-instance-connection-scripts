5ea02002ecc4de0de3f614113fbde805ae21c200
Kyle Wieser <90803891+KyleWieser@users.noreply.github.com>
2022-06-28 16:15:36 -0700

    Add files via upload

--- secret_tunnel.bat
+++ secret_tunnel.bat
@@ -1,17 +1,21 @@
 @echo off
 
-
+setlocal
 set ALPHA_IP=54.243.148.13
 set BETA_IP=52.203.76.119
 set GAMMA_IP=44.206.221.84
 set DELTA_IP=44.207.17.207
+set VNC_PASSWORD=chilicon
+
 
 call :instance_choice
 
 echo Connecting to %SELECTED_IP%
 
-ssh -L 5901:localhost:5901 -i chilicon-p2p-support.pem ec2-user@%SELECTED_IP%
+start ssh -L 5901:localhost:5901 -i chilicon-p2p-support.pem ec2-user@%SELECTED_IP%
+vncviewer64-1.11.0.exe localhost:5901
 
+endlocal
 goto :eof
 
 
@@ -22,11 +26,6 @@ echo 3: (Gamma 44.206.221.84^)
 echo 4: (Delta 44.207.17.207^)
 set /p selectedinstance=Enter the number of the EC2 instance you wish to connect to: 
 echo %selectedinstance%
-
-if %selectedinstance%==1 set SELECTED_IP=%ALPHA_IP%
-if %selectedinstance%==2 set SELECTED_IP=%BETA_IP%
-if %selectedinstance%==3 set SELECTED_IP=%GAMMA_IP%
-if %selectedinstance%==4 set SELECTED_IP=%GAMMA_IP%
 
 if %selectedinstance%==1 (set SELECTED_IP=%ALPHA_IP%
 )	else (
